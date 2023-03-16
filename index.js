33840cbff4087723dbfd21a5c01ce442f4a9e7b6
MSchuur <mschuur31@gmail.com>
2023-03-16 10:25:53 -0300

    Fixed all the licenses to the correct version IAW the create Github Repsoitory list

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const readmeQuestions = [
         type: 'list',
         message: 'Choose the license that your are using',
         name: 'license',
-        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BDS 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Softwae License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0',  'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Softwae License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0',  'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
     },
 ];
 
