2936aa11e756eef22575e63d49aa4af1a95de2b2
MSchuur <mschuur31@gmail.com>
2023-03-16 14:31:53 -0300

    License section automated text and license link created and tested

--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -2,32 +2,55 @@
 const renderLicenseBadge = (license) => {
  const licenseBadges = {
     'None': ' ',
-    'Apache License 2.0': '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
-    'GNU General Public License v3.0': '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
-    'MIT License' :'[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-    'BSD 2-Clause "Simplified" License' : '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)',
-    'BSD 3-Clause "New" or "Revised" License' : '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
-    'Boost Softwae License 1.0': '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
-    'Creative Commons Zero v1.0 Universal' : '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)',
-    'Eclipse Public License 2.0' : '[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)',
-    'GNU Affero General Public License v3.0' : '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
-    'GNU General Public License v2.0' : '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
-    'GNU Lesser General Public License v2.1' : '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v2.1-blue.svg)](https://www.gnu.org/licenses/lgpl-2.1)',
-    'Mozilla Public License 2.0' : '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
-    'The Unlicense' : '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+    'Apache License 2.0': '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)',
+    'GNU General Public License v3.0': '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    'MIT License' :'![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+    'BSD 2-Clause "Simplified" License' : '![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)',
+    'BSD 3-Clause "New" or "Revised" License' : '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)',
+    'Boost Softwae License 1.0' : '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)',
+    'Creative Commons Zero v1.0 Universal' : '![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)',
+    'Eclipse Public License 2.0' : '![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)',
+    'GNU Affero General Public License v3.0' : '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)',
+    'GNU General Public License v2.0' : '![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)',
+    'GNU Lesser General Public License v2.1' : '![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v2.1-blue.svg)',
+    'Mozilla Public License 2.0' : '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)',
+    'The Unlicense' : '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)'
   }
   return licenseBadges[license]
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Renders the appropriate licnese badge depending on the license chossen by the user during the CLI input
+const renderLicenseLink = (license) => {
+  const licenseLinks = {
+    'None': ' ',
+    'Apache License 2.0': '(https://opensource.org/licenses/Apache-2.0)',
+    'GNU General Public License v3.0': '(https://www.gnu.org/licenses/gpl-3.0)',
+    'MIT License' :'(https://opensource.org/licenses/MIT)',
+    'BSD 2-Clause "Simplified" License' : '(https://opensource.org/licenses/BSD-2-Clause)',
+    'BSD 3-Clause "New" or "Revised" License' : '(https://opensource.org/licenses/BSD-3-Clause)',
+    'Boost Softwae License 1.0': '(https://www.boost.org/LICENSE_1_0.txt)',
+    'Creative Commons Zero v1.0 Universal' : '(http://creativecommons.org/publicdomain/zero/1.0/)',
+    'Eclipse Public License 2.0' : '(https://opensource.org/licenses/EPL-2.0)',
+    'GNU Affero General Public License v3.0' : '(https://www.gnu.org/licenses/agpl-3.0)',
+    'GNU General Public License v2.0' : '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
+    'GNU Lesser General Public License v2.1' : '(https://www.gnu.org/licenses/lgpl-2.1)',
+    'Mozilla Public License 2.0' : '(https://opensource.org/licenses/MPL-2.0)',
+    'The Unlicense' : '(http://unlicense.org/)'
+  }
+  return licenseLinks[license]
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Renders the appropriate licnese badge depending on the license chossen by the user during the CLI input
+const renderLicenseSection = (license) => {
+  if(license === 'None') {
+    return `This project does not have a license attached.`
+  }
+  else {
+    return `This project is Licensed under the ${license} license. For more information on the license click the following link. ${renderLicenseLink(license)}`
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Generate the README.md from the CLI input
 const generateMarkdown = (data) =>{
   return `
 # ${data.title}
@@ -53,7 +76,7 @@ ${data.installation}
 ${data.usage}
 
 ## License
-${data.license}
+${renderLicenseSection(data.license)}
 
 ## Contributing
 ${data.contributing}
